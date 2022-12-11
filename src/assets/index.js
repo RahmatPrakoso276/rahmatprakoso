7a0e155c16cf05cfeda5382aeb7cec87735d3338
Rahmat Prakoso <79794844+RahmatPrakoso276@users.noreply.github.com>
2022-12-11 23:36:13 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,6 @@ import binance from "./binance.png";
 import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./logo.png";
 import fb from "./fb.png";
 import git from "./git.png";
 import linkedins from "./linkedin.png";
@@ -46,7 +45,7 @@ export {
   card,
   coinbase,
   dropbox,
-  logo,
+
   quotes,
   robot,
   fotorahmat,
